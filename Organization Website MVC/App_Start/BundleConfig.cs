a58ffe63e926efc59e9bb77606c93dfe7017f484
Aiion <alex.soderberg97@gmail.com>
2016-10-24 22:19:42 +0200

    [Org Web v2] added style.css to BundleConfig

--- Organization Website MVC/App_Start/BundleConfig.cs
+++ Organization Website MVC/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace Organization_Website_MVC
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/Style.css"));
         }
     }
 }
