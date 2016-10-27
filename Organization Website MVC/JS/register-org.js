592a57c3e2cb3a659e918530f6d1392c1640eb37
Felix Wetell <felix.wetell@gmail.com>
2016-10-27 08:57:32 +0200

    [Org Web v2] Added a list in jumbotron for organizations

---
+++ Organization Website MVC/JS/register-org.js
@@ -0,0 +1,8 @@
+﻿function Alert (){
+    if (document.getElementById("name-input") == "") {
+        document.getElementById("alert-org").style.display = "block";
+    }
+    else {
+        document.getElementById("alert-org").style.display = "block";
+    }
+}
