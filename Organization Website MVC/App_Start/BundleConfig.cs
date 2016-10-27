6552107f69bbd1ff04b04be9cb15fd14e87f69f5
Aiion <alex.soderberg97@gmail.com>
2016-10-27 10:21:29 +0200

    [org web v2] updated bundle.config

--- Organization Website MVC/App_Start/BundleConfig.cs
+++ Organization Website MVC/App_Start/BundleConfig.cs
@@ -26,7 +26,7 @@ namespace Organization_Website_MVC
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/Style.css"));
+                      "~/Content/layout.css"));
         }
     }
 }
