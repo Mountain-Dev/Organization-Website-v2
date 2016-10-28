1f9f2c954e970c36f2d6e5b484dc053baba715c8
Felix Wetell <felix.wetell@gmail.com>
2016-10-28 10:31:04 +0200

    Added organization list to "Organizations" and a introduction on start page

--- Organization Website MVC/Controllers/HomeController.cs
+++ Organization Website MVC/Controllers/HomeController.cs
@@ -29,15 +29,9 @@ namespace Organization_Website_MVC.Controllers
             return View(allData);
         }
 
-
-        public ActionResult Index_dev()
-        {
-            return View(allData);
-        }
-
         public ActionResult Organizations()
         {
-            return View();
+            return View(allData);
         }
 
 
